58d585afba22cb7345ff91d6f84beb0b0a300e11
Robert <poprobert1999@yahoo.com>
2024-08-05 18:30:24 +0300

    Exercitiu nou pe site ul formy project

--- Sesiunea_3_Selectori_P3/exercitii.py
+++ Sesiunea_3_Selectori_P3/exercitii.py
@@ -2,8 +2,9 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import random
 import time
-#Exercitii XPATH
+#Exercitii XPATH pe site ul formy
 chrome = webdriver.Chrome()
 
 chrome.maximize_window()
 chrome.get("https://formy-project.herokuapp.com/form")
+chrome.find_element(By.XPATH, "//input[@id=\"first-name\"]").send_keys("Robert")#Adaugam numele Robert la First name
